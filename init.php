2fa39a66abfffd3b67da6955a96f527016b3a9e1
Matthew J. Sahagian <msahagian@dotink.org>
2015-10-20 00:31:57 -0700

    Adding .env support

--- init.php
+++ init.php
@@ -1,7 +1,9 @@
 <?php namespace Inkwell
 {
 	use Affinity;
-	use Auryn;
+	use Auryn\Injector;
+	use Dotenv\Dotenv;
+	use InvalidArgumentException;
 
 	$loader    = require 'vendor/autoload.php';
 	$constants = require 'constants.php';
@@ -10,8 +12,14 @@
 		define('IW\\' . $const, $value);
 	}
 
-	$app  	       = new Core(realpath(__DIR__));
-	$broker        = new Auryn\Injector();
+	$app  	= new Core(realpath(__DIR__));
+	$broker = new Injector();
+
+	if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . '.env')) {
+		$dotenv = new Dotenv(__DIR__);
+		$dotenv->load();
+	}
+
 	$config_dir    = $app->getDirectory($app->getEnvironment('IW_CONFIG_ROOT', 'config'));
 	$action_dir    = $app->getDirectory($app->getEnvironment('IW_ACTION_ROOT', 'include'));
 	$environment   = $app->getEnvironment('IW_ENVIRONMENT', 'prod');
