235c2b12aefee5e603e6d28024ebb305dcc2e7bc
Matthew J. Sahagian <msahagian@dotink.org>
2015-10-24 15:56:23 -0700

    Fixed up Person model and People formatting

--- user/repositories/Sandstorm/People.php
+++ user/repositories/Sandstorm/People.php
@@ -1,11 +1,9 @@
-<?php namespace Sandstorm {
-
+<?php namespace Sandstorm
+{
 	use Inkwell\Doctrine\Repository;
 
 	class People extends Repository
 	{
 		const MODEL = 'Sandstorm\Person';
-
 	}
-
 }
