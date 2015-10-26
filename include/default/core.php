cb8a21ff9ab49c9ffeef7f73daf10a05ad555e3b
Matthew J. Sahagian <msahagian@dotink.org>
2015-10-26 11:07:00 -0700

    Composer update + updated env example

--- include/default/core.php
+++ include/default/core.php
@@ -1,59 +1,14 @@
 <?php
 
-	use Whoops\Handler\PrettyPageHandler;
-	use Whoops\Handler\PlainTextHandler;
-	use Whoops\Handler\JsonResponseHandler;
-
 	return Affinity\Action::create(function($app, $broker) {
 
 		//
 		// Setup execution mode and debugging
 		//
 
-		error_reporting(E_ALL);
-		ini_set('display_errors', 0);
-		ini_set('display_startup_errors', 0);
+		$server_admin = $app->getEnvironment('SERVER_ADMIN', 'root');
 
-		$server_admin      = $app->getEnvironment('SERVER_ADMIN', 'root');
-		$debugging         = $app['engine']->fetch('core', 'debugging', []);
-		$execution_mode    = $app['engine']->fetch('core', 'execution_mode', IW\EXEC_MODE\PRODUCTION);
-
-		if (!isset($debugging['destination'])) {
-			$debugging['destination'] = $execution_mode != IW\EXEC_MODE\PRODUCTION
-				? IW\ERROR\DESTINATION_RESPONSE
-				: IW\ERROR\DESTINATION_NULL;
-		}
-
-		if ($debugging['destination'] != IW\ERROR\DESTINATION_NULL) {
-			$debug_manager = new Whoops\Run;
-
-			switch ($debugging['destination']) {
-				case IW\ERROR\DESTINATION_RESPONSE:
-					if ($app->checkSAPI('cli', 'embed')) {
-						$debug_handler = new PlainTextHandler();
-
-					} else {
-						$debug_handler = new PrettyPageHandler();
-						$debug_handler->setPageTitle("Whoops! There was a problem.");
-
-					}
-
-					$debug_manager->pushHandler($debug_handler);
-					break;
-
-				default:
-					$debug_manager->pushHandler(function($exception, $inspector, $manager){
-						//
-						// @todo E-mail
-						//
-					});
-					break;
-			}
-
-			$debug_manager->register();
-		}
-
-		$app->setExecutionMode($execution_mode);
+		$app->setExecutionMode($app['engine']->fetch('core', 'execution_mode', IW\EXEC_MODE\PRODUCTION));
 		$app->setWriteDirectory($app['engine']->fetch('core', 'write_directory', 'writable'));
 
 		//
